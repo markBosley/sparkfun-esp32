66ecedb132069a3af1ef69e0c8e522d535c94397
markBosley <mark.bosley@brikeda.com>
2021-08-24 12:48:33 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int ledPin = 5;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    Serial.println("Hello, sparkfun fans!");
+  //  digitalWrite(ledPin, HIGH);
+    delay(500);
+  //  digitalWrite(ledPin, LOW);
+    delay(500);
+}
