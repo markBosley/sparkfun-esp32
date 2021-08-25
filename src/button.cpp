89cb6e3af2dd254168b6cf62c3e401dac87346a7
markBosley <mark.bosley@brikeda.com>
2021-08-25 12:21:34 -0700

    button pressed

---
+++ src/button.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+int ledPin = 5;
+const int BUTTON_PIN = 0;
+
+void setup()
+{
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    if (digitalRead(BUTTON_PIN) == LOW)
+  {
+     // while (digitalRead(BUTTON_PIN) == LOW)  ; // Wait for button to be released
+      Serial.println("button is pressed! yay!");
+  }
+    
+    
+  //Serial.println("hello!");
+    delay(500);
+}
