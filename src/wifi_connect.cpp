6a4dbb4f5b8c653ec32959d25392168dd68c3a80
markBosley <mark.bosley@brikeda.com>
2021-08-26 09:56:41 -0700

    wifi and webserver

---
+++ src/wifi_connect.cpp
@@ -0,0 +1,36 @@
+//  #include <Arduino.h>
+// #include <WiFi.h>
+
+// const char* ssid     = "MB-brikeda";
+// const char* password = "lpdc533$";
+
+// void setup() {
+//  Serial.begin(115200);     // set the LED pin mode
+
+//     delay(10);
+
+//     // We start by connecting to a WiFi network
+
+//     Serial.println();
+//     Serial.println();
+//     Serial.print("Connecting to ");
+//     Serial.println(ssid);
+
+//     WiFi.begin(ssid, password);
+
+//     while (WiFi.status() != WL_CONNECTED) {
+//         delay(500);
+//         Serial.print(".");
+//     }
+
+//     Serial.println("");
+//     Serial.println("WiFi connected.");
+//     Serial.println("IP address: ");
+//     Serial.println(WiFi.localIP());
+    
+
+// }
+
+// void loop(){
+  
+//   }
